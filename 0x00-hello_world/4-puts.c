c86019eaedfca66a0472027a0ff819859f883311
OgranyaReginald <ogranyareginal>
2022-09-10 15:29:44 -0700

    fff

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retutrn: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"programming is like is building a multilingual puzzle");
+
+	return(0)
+}
