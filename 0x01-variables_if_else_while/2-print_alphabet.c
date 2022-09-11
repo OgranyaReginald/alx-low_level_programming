5ab343b0dc33e6580ef87f2f0c7b3c8510a4fe87
OgranyaReginald <ogranyareginal>
2022-09-11 01:24:19 -0700

    bbb

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (success)
+ */
+int main(void)
+{
+	char 1;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	putchar('\n');
+	retuen (0);
+}
