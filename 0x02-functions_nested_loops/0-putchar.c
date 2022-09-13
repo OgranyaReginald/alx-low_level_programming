83b586474b02c258b09f300f6ed4de0d99ad70c1
OgranyaReginald <ogranyareginal>
2022-09-13 05:36:34 -0700

    ooo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * main _ Entry point
@@ -12,7 +12,7 @@ int main(void)
 {
 	int i = 0;
 	char c;
-	char s[] = "Holberton\n"
+	char s[] = "putchar\n"
 
 	while (i <= 9)
 	{
