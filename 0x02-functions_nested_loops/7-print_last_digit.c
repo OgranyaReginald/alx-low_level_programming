57f4a635e515c7c212a427ee08f0f33b07ac21ad
OgranyaReginald <ogranyareginal>
2022-09-14 12:47:31 -0700

    yyy

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,6 +3,7 @@
 /**
  * prints_last_digit - prints the last digit of a number
  * @n: n is an integer
+ * Description: print_the_last_digit
  * Return: value of last digit 
  */
 int print_last_digit(int n)
