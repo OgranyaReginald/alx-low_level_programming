e75e31370d07ecd201b214510d5b5ea68fef86e0
OgranyaReginald <ogranyareginal>
2022-09-14 13:46:11 -0700

    ttt

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 
 /**
  * main - computes and prints the sum  of all multiples
- * of 3 or 5 belw 1024
+ * of 3 or 5 below 1024
  * Return: Always 0 (success)
  */
 int main(void)
@@ -20,6 +20,6 @@ int main(void)
 			 else if ((i % 5) == 0)
 				sum5 = sum5 + i;
 		sum = sum3 + sum5;
-		printf("%lu\n", sum);
+			printf("%lu\n", sum);
 		return (0);
 }
