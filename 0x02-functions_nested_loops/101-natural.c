4ba2db14ba897c591884f3d2891e768240f7ea03
OgranyaReginald <ogranyareginal>
2022-09-14 13:35:34 -0700

    hhhh

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,9 +15,9 @@ int main(void)
 		sum = 0;
 
 		for (i = 0; 1 < 1024; ++i)
-			if ((i % 3 ) == 0)
+			if ((i % 3) == 0)
 				sum3 = sum3 + i;
-			 else if ((i % 5 ) == 0)
+			 else if ((i % 5) == 0)
 				sum5 = sum5 +i;
 		sum = sum3 + sum5;
 		printf("%lu\n", sum);
