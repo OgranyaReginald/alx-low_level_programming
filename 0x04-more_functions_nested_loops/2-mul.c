5cc830914e53b051f412dcbba58bb295d3d119a9
OgranyaReginald <ogranyareginal>
2022-09-16 14:55:37 -0700

    ooo

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * mul - multiples of two integers
+ * @a: number1
+ * @b: number2
+ * Return: The multiplication result of the numbers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
